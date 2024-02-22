2281d56147c7c7178104185996ab3ebc844921d5
yomgoun <ygy718500@gmail.com>
2024-02-22 10:14:05 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : CharacterController
+{
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        // Debug.Log("OnMove" + value.ToString());
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        // Debug.Log("OnLook" + value.ToString());
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        // Vector 값을 실수로 변환
+        {
+            CallLookEvent(newAim);
+        }
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
+    }
+}
